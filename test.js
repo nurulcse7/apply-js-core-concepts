d4713a72a3f3ed1a251a2555a4f96e309f6b138a
Nurul Islam <nurulbdpersonal@gmail.com>
2022-08-06 15:34:20 +0600

    test.js file added

---
+++ test.js
@@ -0,0 +1,11 @@
+// var numbers = [5, 6, 7, 8, 9, 10, 11, 12]
+
+function getSumOfAnArray(numbers){
+ for(let i = 0; i < numbers.length; i++){
+    const index = i;
+    console.log(index)
+ }
+}
+
+const myNumbers = [5, 6, 7, 8, 9, 10, 11, 12]
+getSumOfAnArray(myNumbers)
